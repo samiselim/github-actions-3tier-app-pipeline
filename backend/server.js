832e1a0e94cfb5461f3d6ef1563460828097ae63
sami abdelaziz selim <samiselim75@gmail.com>
2024-06-14 16:37:29 +0300

    Editing the deployment files and Docker files

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const mongoose = require("mongoose");
-const port = 3001;
+const port = 8080;
 const routes = require("./routes");
 require('dotenv').config(); 
 
--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -13,7 +13,7 @@ import {
 } from "reactstrap";
 import TodoForm from "./todo-form";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL ='http://backend-service:8080/api';
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
