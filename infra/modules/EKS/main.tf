6b20c8520154dc49a75f7100d54ac6eed3ec011c
sami abdelaziz selim <samiselim75@gmail.com>
2024-06-14 16:01:44 +0300

    Adding some Addons to EKS

--- infra/modules/EKS/main.tf
+++ infra/modules/EKS/main.tf
@@ -54,7 +54,7 @@ resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
 resource "aws_eks_cluster" "eks" {
   name     = "fp-cluster"
   role_arn = aws_iam_role.eks_cluster_role.arn
-  
+ 
   vpc_config {
     subnet_ids = var.subnet_ids
   }
@@ -70,7 +70,7 @@ resource "aws_eks_node_group" "node_group" {
   node_role_arn   = aws_iam_role.eks_worker_node_role.arn
   subnet_ids      = var.worker_subnet_ids
   instance_types = ["t3.medium"]
-
+  
   scaling_config {
     desired_size = 2
     max_size     = 5
@@ -87,19 +87,19 @@ resource "aws_eks_node_group" "node_group" {
 
   ]
 }
-# resource "aws_eks_addon" "cnii" {
-#   cluster_name = aws_eks_cluster.eks.name
-#   addon_name   = "vpc-cni"
-# }
-# resource "aws_eks_addon" "coredns" {
-#   cluster_name                = aws_eks_cluster.eks.name
-#   addon_name                  = "coredns"
-# }
-# resource "aws_eks_addon" "kube-proxy" {
-#   cluster_name                = aws_eks_cluster.eks.name
-#   addon_name                  = "kube-proxy"
-# }
-# resource "aws_eks_addon" "aws-ebs-csi-driver" {
-#   cluster_name                = aws_eks_cluster.eks.name
-#   addon_name                  = "aws-ebs-csi-driver"
-# }
+resource "aws_eks_addon" "cnii" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "vpc-cni"
+}
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "coredns"
+}
+resource "aws_eks_addon" "kube-proxy" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "kube-proxy"
+}
+resource "aws_eks_addon" "aws-ebs-csi-driver" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "aws-ebs-csi-driver"
+}
