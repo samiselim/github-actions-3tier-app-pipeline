2b3139e63c838fd177d0108028c74be42a64ec9e
Sami AbdElAziz Selim <49785198+samiselim@users.noreply.github.com>
2024-08-05 13:06:34 +0300

    Update main.tf | adding helm release for alb ingress controller resource

--- infra/main.tf
+++ infra/main.tf
@@ -26,4 +26,32 @@ module "eks" {
   subnet_ids = concat(module.vpc.private_subnet_ids , module.vpc.public_subnet_ids)
   worker_subnet_ids = module.vpc.public_subnet_ids
  }
+resource "helm_release" "alb_ingress_controller" {
+  name       = "alb-ingress-controller"
+  repository = "https://kubernetes-sigs.github.io/aws-load-balancer-controller"
+  chart      = "aws-load-balancer-controller"
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+  set {
+    name  = "region"
+    value = "eu-west-3"
+  }
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+}
 
+
+# resource "kubernetes_manifest" "nginx" {
+#   manifest = yamldecode(file("./k8s/nginx.yaml"))
+# }
+# resource "kubernetes_manifest" "nginx-service" {
+#   manifest = yamldecode(file("./k8s/nginx-service.yaml"))
+# }
+
+# resource "kubernetes_manifest" "ingress" {
+#   manifest = yamldecode(file("./k8s/ingress.yaml"))
+# }
